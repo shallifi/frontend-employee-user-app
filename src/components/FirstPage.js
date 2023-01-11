fa7971fb96d7a2d8af478473921b067140c6046d
Benjamin <shallifi@hotmail.com>
2023-01-10 20:56:25 -0500

    added onchange event

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -1,17 +1,35 @@
 import React from 'react'
+import {useState} from 'react';
 // import Button from 'react-bootstrap/Button';
 // import ButtonGroup from 'react-bootstrap/ButtonGroup';
 // import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 
 function FirstPage() {
+  const [firstName, setFirstName] = useState ('');
+  const [lastName, setLastName] = useState ('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('First name: ${firstName}');
+    console.log('Last name: ${lastName}')
+  }
+
+
   return (
     <div className="App">
-    <form>
+    <form onSubmit={handleSubmit}>
           
         First Name:
-        <input type="text" name="name" />
+        <input type="text"
+          value={firstName}
+          onChange={event => setFirstName(event.target.value)}
+        />
         Last Name:
-        <input type="text" name="name" />
+        <input type="text" 
+        value={lastName}
+        onChange={event => setLastName(event.target.value)}
+        />
+        
         <div className='radiobutton'>
          
           {/* New to SCC?
