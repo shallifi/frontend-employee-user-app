d250f1e1c114f92ea760c4c256003db7e78ceb7f
Benjamin <shallifi@hotmail.com>
2023-03-28 23:14:37 -0400

    able to get port working but not submit

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -1,25 +1,58 @@
 import React from 'react'
 import {useState} from 'react';
+// import useForm from '../hooks/useForm';
+
 // import { json } from 'react-router-dom';
 // import Button from 'react-bootstrap/Button';
 // import ButtonGroup from 'react-bootstrap/ButtonGroup';
 // import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 
 function FirstPage() {
-  const [firstName, setFirstName] = useState ('');
-  const [lastName, setLastName] = useState ('');
+  const [firstName, setFirstName] = useState ({
+    first_name: ''
+  });
+  const [lastName, setLastName] = useState ({
+    last_name: ''
+  });
+
+
+
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+    console.log(name, value);
+    if (name === 'firstName') {
+      setFirstName(value);
+    } else if (name === 'lastName') {
+      setLastName(value);
+    }
+  }
+
+
+  // const handleChange = (e) => {
+  //   const {name, value} = e.target;
+  //   console.log(name, value);
+  //   setFirstName((firstName) => ({...firstName, [name]: value}));
+  //   setLastName((lastName) => ({...lastName, [name]: value}));
+  // }
+
+
+
+
+
+// handleSubmit function is not working, cors, failed to fetch, need to fix the handleChange function. First name on change is happening but not staying on screen and not submitting to the database. how to integrate useform into this function?
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(`First name: ${firstName}`);
-    console.log(`Last name: ${lastName}`);
+    // console.log(`First name: ${firstName}`);
+    // console.log(`Last name: ${lastName}`);
     const data = {firstName, lastName};
     console.log(data);
 
     fetch('http://localhost:3000/employees', {
       method: 'POST',
       headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
+      credentials: 'include'
     })
     .then(response => response.json())
     .then(data => { console.log('Success:', data); })
@@ -34,11 +67,17 @@ function FirstPage() {
     <form onSubmit={handleSubmit}>
     <label>
         First name:
-        <input type="text" value={firstName} onChange={(event) => setFirstName(event.target.value)} />
+        <input type="text"
+        name="firstName" 
+        value={firstName} 
+        onChange={handleChange} />
       </label>
       <label>
         Last name:
-        <input type="text" value={lastName} onChange={(event) => setLastName(event.target.value)} />
+        <input type="text" 
+        name="lastName"
+        value={lastName}
+        onChange={handleChange} />
       </label>
 
 {/* 
