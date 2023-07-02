565c786bcf8ce7f9d69d51c9504d6be82daa475e
Benjamin <shallifi@hotmail.com>
2023-07-01 23:03:31 -0400

    office location info commented out but left there in case

--- src/components/EmpModal.js
+++ src/components/EmpModal.js
@@ -14,8 +14,10 @@ const EmpModal = ({ employee, showModal, handleCloseModal }) => {
             <p>Name: {employee.first_name} {employee.last_name}</p>
             {/* <p>Last Name: {employee.last_name}</p> */}
             <p>Title: {employee.title?.title_name || 'N/A'}</p>
-            <p>Office: {employee.office?.office_name || 'N/A'}</p>
-            <p>Extension: {employee.extension}</p>
+            <p>Office: {employee.office?.office_name || 'N/A'} </p>
+            {/* <p>{employee.office?.office_phone} </p>
+            <p> {employee.office?.address}</p> */}
+             <p>Extension: {employee.extension}</p>
             <p>Supervisor: {employee.supervisor ? `${employee.supervisor.first_name} ${employee.supervisor.last_name}` : 'N/A'}</p>
             {/* Add more employee fields as needed */}
           </div>
