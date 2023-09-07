7f944719eca67cad6752c91e02c89c4c9c88ba91
Benjamin <shallifi@hotmail.com>
2023-09-06 21:23:33 -0400

    cleaned up CustomeFilter

--- src/components/CustomFilter.js
+++ src/components/CustomFilter.js
@@ -5,12 +5,12 @@ import React from 'react';
 function CustomFilter({ column }) {
   const { filterValue, setFilter } = column;
 
-    // const count = column.preFilteredRows.length;
+   
   return (
     <input
       value={filterValue || ''}
       onChange={(e) => setFilter(e.target.value)}
-    //   placeholder={`"Search ${count} ..."`}
+  
         placeholder={`Search ...`}
     />
   );
